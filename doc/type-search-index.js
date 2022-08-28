0ed70bc1c3685d680e2f34a700eb9775fea88724
Michael Cotterell <mepcotterell@gmail.com>
2022-08-27 21:09:44 -0400

    update api docs

--- doc/type-search-index.js
+++ doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"cs1302.gameutil","l":"Token"},{"p":"cs1302.gameutil","l":"TokenGrid"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"cs1302.gameutil","l":"GamePhase"},{"p":"cs1302.gameutil","l":"Token"},{"p":"cs1302.gameutil","l":"TokenGrid"}];updateSearchResults();
