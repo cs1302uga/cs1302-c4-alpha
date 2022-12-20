ed11644c2055cfe7fe69860002bf306944cb8b01
Brad Barnes <bjb211@users.noreply.github.com>
2022-12-20 13:17:50 -0500

    Update ConnectFour.java

--- src/cs1302/game/ConnectFour.java
+++ src/cs1302/game/ConnectFour.java
@@ -1,149 +1,279 @@
 package cs1302.game;
 
-import java.util.Scanner;
-import java.io.FileNotFoundException;
-import java.io.File;
-
-import cs1302.game.ConnectFour;
 import cs1302.gameutil.GamePhase;
 import cs1302.gameutil.Token;
+import cs1302.gameutil.TokenGrid;
 
 /**
- * A class to allow for CLI gameplay of a connect four game.
+ * {@code ConnectFour} represents a two-player connection game involving a two-dimensional grid of
+ * {@linkplain cs1302.gameutil.Token tokens}. When a {@code ConnectFour} game object is
+ * constructed, several instance variables representing the game's state are initialized and
+ * subsequently accessible, either directly or indirectly, via "getter" methods. Over time, the
+ * values assigned to these instance variables should change so that they always reflect the
+ * latest information about the state of the game. Most of these changes are described in the
+ * project's <a href="https://github.com/cs1302uga/cs1302-c4-alpha#functional-requirements">
+ * functional requirements</a>.
  */
-public class ConnectFourCLI {
+public class ConnectFour {
 
-    private static final Scanner STD_IN = new Scanner(System.in);
+    //----------------------------------------------------------------------------------------------
+    // INSTANCE VARIABLES: You should NOT modify the instance variable declarations below.
+    // You should also NOT add any additional instance variables. Static variables should
+    // also NOT be added.
+    //----------------------------------------------------------------------------------------------
+
+    private int rows;        // number of grid rows
+    private int cols;        // number of grid columns
+    private Token[][] grid;  // 2D array of tokens in the grid
+    private Token[] player;  // 1D array of player tokens (length 2)
+    private int numDropped;  // number of tokens dropped so far
+    private int lastDropRow; // row index of the most recent drop
+    private int lastDropCol; // column index of the most recent drop
+    private GamePhase phase; // current game phase
+
+    //----------------------------------------------------------------------------------------------
+    // CONSTRUCTOR
+    //----------------------------------------------------------------------------------------------
 
     /**
-     * Entry point to the {@link cs1302.game.ConnectFourDriver} program.
-     * @param args the command-line arguments (not used)
-     */
-    public static void main(String[] args) {
-        ConnectFourCLI.printWelcome();
-        ConnectFour game = ConnectFourCLI.manualSetUp();
-        ConnectFourCLI.play(game);
-    } // main
-
-    /**
-     * If there is not file path specified then the user manually sets up a connect four
-     * game by specifiying the number of rows, columns, and colors for both player tokens.
+     * Constructs a {@link cs1302.game.ConnectFour} game with a grid that has {@code rows}-many
+     * rows and {@code cols}-many columns. All of the game's instance variables are expected to
+     * be initialized by this constructor as described in the project's
+     * <a href="https://github.com/cs1302uga/cs1302-c4-alpha#functional-requirements">functional
+     * requirements</a>.
      *
-     *@return game a {@code ConnectFour} object.
+     * @param rows the number of grid rows
+     * @param cols the number of grid columns
+     * @throws IllegalArgumentException if the value supplied for {@code rows} or {@code cols} is
+     *     not supported. The following values are supported: {@code 6 <= rows <= 9} and
+     *     {@code 7 <= cols <= 9}.
      */
-    public static ConnectFour manualSetUp() {
+    public ConnectFour(int rows, int cols)  {
+        //
+        // replace the entire contents of this constructor with your implementation
+        //
+        //throw new UnsupportedOperationException("constructor: not yet implemented.");
+    } // ConnectFour
 
-        while (true) {
-            try {
-                System.out.print("\nPlease enter the number of row for the board (6-9): ");
-                int rows = Integer.parseInt(STD_IN.nextLine());
-                System.out.print("\nPlease enter the number of columns for the board (7-9): ");
-                int columns = Integer.parseInt(STD_IN.nextLine());
-                ConnectFour game = new ConnectFour(rows,columns);
-                System.out.print("\nEnter a color for Player 1's token " +
-                    "(BLUE, CYAN, GREEN, PURPLE, RED, YELLOW): ");
-                Token player1 = Token.valueOf(STD_IN.nextLine().toUpperCase());
-                System.out.print("\nEnter a color for Player 2's token " +
-                    "(BLUE, CYAN, GREEN, PURPLE, RED, YELLOW): ");
-                Token player2 = Token.valueOf(STD_IN.nextLine().toUpperCase());
-                game.setPlayerTokens(player1, player2);
-                return game;
-            } catch (NumberFormatException nfe) {
-                System.out.println("\n*** Rows and columns must be valid integers. ***\n");
-                nfe.printStackTrace();
-            } catch (IllegalArgumentException iae) {
-                System.out.println(
-                    "\n*** Could not construct the game. " +
-                    "Either the number of rows/cols is too large/small " +
-                    "or a selected player color is invalid. ***\n");
-                iae.printStackTrace();
-            } catch (IllegalStateException ise) {
-                System.out.println(
-                    "\n*** Game must be in the NEW phase when initially " +
-                    "setting the player tokens. ***\n");
-            } // try
-        } // while
-    } // manualSetUp
+    //----------------------------------------------------------------------------------------------
+    // INSTANCE METHODS
+    //----------------------------------------------------------------------------------------------
 
     /**
-     * Print all lines from the file.
-     * @param filename the file to print
-     */
-    public static void printFileLines(String filename) {
-        try {
-            File inFile = new File(filename);
-            Scanner input = new Scanner(inFile);
-            while (input.hasNextLine()) {
-                System.out.println(input.nextLine());
-            }
-            input.close();
-        } catch (FileNotFoundException fnfe) {
-            System.out.println("Missing " + filename + ". Make sure it is in the" +
-                "resource folder in the projects home directory.");
-            fnfe.printStackTrace();
-            System.exit(1);
-        } // try
-    } // printFileLines
-
-    /**
-     * prints the 'Connect Four' header from the 'resources/connectfour.txt' file.
-     */
-    public static void printHeader() {
-        printFileLines("resources/connectfour.txt");
-    } // printHeader
-
-    /**
-     * prints the 'Welcome' banner from the 'resources/welcome.txt' file.
-     */
-    public static void printWelcome() {
-        printFileLines("resources/welcome.txt");
-        System.out.println();
-        ConnectFourCLI.printHeader();
-    } // printWelcome
-
-    /**
-     * prints the 'Game Over' banner from the 'resources/gameover.txt' file.
-     */
-    public static void printGameOver() {
-        printFileLines("resources/gameover.txt");
-    } // printGameOver
-
-    /**
-     * A method that plays a {@code ConnectFour} object. Switches between players to collect
-     * the column to drop a token in with the {@code dropToken method}. Method loops until
-     * {@code isLastDropConnectFour} returns true indicating the game is over.
+     * Return the number of rows in the game's grid.
      *
-     * @param game a {@code ConnectFour} game to be played.
+     * @return the number of rows
      */
-    public static void play(ConnectFour game) {
-        int player = 0;
-        boolean isWon = false;
+    public int getRows() {
+        //
+        // replace the entire contents of this method with your implementation
+        //
+        throw new UnsupportedOperationException("getRows: not yet implemented.");
+    } // getRows
 
-        while (game.getPhase() != GamePhase.OVER) {
-            try {
-                ConnectFourCLI.printHeader();
-                System.out.println();
-                game.printGrid();
-                System.out.print("\nPlayer " + player + ", enter a column to drop your token: ");
-                int col = Integer.parseInt(STD_IN.nextLine().trim());
-                game.dropToken(player, col);
-                player = (player + 1) % 2;
-                isWon = game.isLastDropConnectFour();
-            } catch (IndexOutOfBoundsException e) {
-                System.out.println("\n *** Invalid column entry, try again ***");
-                e.printStackTrace();
-            } catch (IllegalStateException ise) {
-                System.out.println("\n *** Token cannot be dropped either because the column is "
-                                   + "full or the game is not in the READY or PLAYABLE phase");
-                ise.printStackTrace();
-            } // try
-        } // while
-        ConnectFourCLI.printGameOver();
-        game.printGrid();
+    /**
+     * Return the number of columns in the game's grid.
+     *
+     * @return the number of columns
+     */
+    public int getCols() {
+        //
+        // replace the entire contents of this method with your implementation
+        //
+        throw new UnsupportedOperationException("getCols: not yet implemented.");
+    } // getCols
 
-        if (game.isLastDropConnectFour()) {
-            player = (player + 1) % 2;
-            System.out.printf("\n\nCongratulations, player %s. You won!\n\n", player);
-        } // if
-    } // play
-} // ConnectFourCLI
+    /**
+     * Return whether {@code row} and {@code col} specify a location inside this game's grid.
+     *
+     * @param row the position's row index
+     * @param col the positions's column index
+     * @return {@code true} if {@code row} and {@code col} specify a location inside this game's
+     *     grid and {@code false} otherwise
+     */
+    public boolean isInBounds(int row, int col) {
+        //
+        // replace the entire contents of this method with your implementation
+        //
+        throw new UnsupportedOperationException("isInBounds: not yet implemented.");
+    } // isInBounds
+
+    /**
+     * Return the grid {@linkplain cs1302.gameutil.Token token} located at the specified position
+     * or {@code null} if no token has been dropped into that position.
+     *
+     * @param row the token's row index
+     * @param col the token's column index
+     * @return the grid token located in row {@code row} and column {@code col}, if it exists;
+     *     otherwise, the value {@code null}
+     * @throws IndexOutOfBoundsException if {@code row} and {@code col} specify a position that is
+     *     not inside this game's grid.
+     */
+    public Token getTokenAt(int row, int col) {
+        //
+        // replace the entire contents of this method with your implementation
+        //
+        throw new UnsupportedOperationException("getTokenAt: not yet implemented.");
+    } // getTokenAt
+
+    /**
+     * Set the first player token and second player token to {@code token0} and {@code token1},
+     * respectively. If the current game phase is {@link cs1302.gameutil.GamePhase#NEW}, then
+     * this method changes the game phase to {@link cs1302.gameutil.GamePhase#READY}, but only
+     * if no exceptions are thrown.
+     *.
+     * @param token0 token for first player
+     * @param token1 token for second player
+     * @throws NullPointerException if {@code token0} or {@code token1} is {@code null}.
+     * @throws IllegalArgumentException if {@code token0 == token1}.
+     * @throws IllegalStateException if {@link #getPhase getPhase()} returns
+     *     {@link cs1302.gameutil.GamePhase#PLAYABLE} or {@link cs1302.gameutil.GamePhase#OVER}.
+     */
+    public void setPlayerTokens(Token token0, Token token1) {
+        //
+        // replace the entire contents of this method with your implementation
+        //
+        throw new UnsupportedOperationException("setPlayerTokens: not yet implemented.");
+    } // setPlayerTokens
+
+    /**
+     * Return a player's token.
+     *
+     * @param player the player ({@code 0} for first player and {@code 1} for second player)
+     * @return the token for the specified player
+     * @throws IllegalArgumentException if {@code player} is neither {@code 0} nor {@code 1}
+     * @throws IllegalStateException if {@link #getPhase getPhase()} returns
+     *     {@link cs1302.gameutil.GamePhase#NEW}.
+     */
+    public Token getPlayerToken(int player) {
+        //
+        // replace the entire contents of this method with your implementation
+        //
+        throw new UnsupportedOperationException("getPlayerToken: not yet implemented.");
+    } // getPlayerToken
+
+    /**
+     * Return the number of tokens that have been dropped into this game's grid so far.
+     *
+     * @return the number of dropped tokens
+     * @throws IllegalStateException if {@link #getPhase getPhase()} returns
+     *     {@link cs1302.gameutil.GamePhase#NEW} or {@link cs1302.gameutil.GamePhase#READY}.
+     */
+    public int getNumDropped() {
+        //
+        // replace the entire contents of this method with your implementation
+        //
+        throw new UnsupportedOperationException("getNumDropped: not yet implemented.");
+    } // getNumDropped
+
+    /**
+     * Return the row index of the last (i.e., the most recent) token dropped into this
+     * game's grid.
+     *
+     * @return the row index of the last drop
+     * @throws IllegalStateException if {@link #getPhase getPhase()} returns
+     *     {@link cs1302.gameutil.GamePhase#NEW} or {@link cs1302.gameutil.GamePhase#READY}.
+     */
+    public int getLastDropRow() {
+        //
+        // replace the entire contents of this method with your implementation
+        //
+        throw new UnsupportedOperationException("getLastDropRow: not yet implemented.");
+    } // getLastDropRow
+
+    /**
+     * Return the col index of the last (i.e., the most recent) token dropped into this
+     * game's grid.
+     *
+     * @return the column index of the last drop
+     * @throws IllegalStateException if {@link #getPhase getPhase()} returns
+     *     {@link cs1302.gameutil.GamePhase#NEW} or {@link cs1302.gameutil.GamePhase#READY}.
+     */
+    public int getLastDropCol() {
+        //
+        // replace the entire contents of this method with your implementation
+        //
+        throw new UnsupportedOperationException("getLastDropCol: not yet implemented.");
+    } // getLastDropCol
+
+    /**
+     * Return the current game phase.
+     *
+     * @return current game phase
+     */
+    public GamePhase getPhase() {
+        //
+        // replace the entire contents of this method with your implementation
+        //
+        throw new UnsupportedOperationException("getPhase: not yet implemented.");
+    } // getPhase
+
+    /**
+     * Drop a player's token into a specific column in the grid. This method should not enforce turn
+     * order -- that is the players' responsibility should they desire an polite and honest game.
+     *
+     * @param player the player ({@code 0} for first player and {@code 1} for second player)
+     * @param col the grid column where the token will be dropped
+     * @throws IndexOutOfBoundsException if {@code col} is not a valid column index
+     * @throws IllegalArgumentException if {@code player} is neither {@code 0} nor {@code 1}
+     * @throws IllegalStateException if {@link #getPhase getPhase()} does not return
+     *    {@link cs1302.gameutil.GamePhase#READY} or {@link cs1302.gameutil.GamePhase#PLAYABLE}
+     * @throws IllegalStateException if the specified column in the grid is full
+     */
+    public void dropToken(int player, int col) {
+        //
+        // replace the entire contents of this method with your implementation
+        //
+        throw new UnsupportedOperationException("dropToken: not yet implemented.");
+    } // dropToken
+
+    /**
+     * Return {@code true} if the last token dropped via {@link #dropToken} created a
+     * <em>connect four</em>. A <em>connect four</em> is a sequence of four equal tokens (i.e., they
+     * have the same color) -- this sequence can occur horizontally, vertically, or diagonally.
+     * If the grid is full or the last drop created a <em>connect four</em>, then this method
+     * changes the game's phase to {@link cs1302.gameutil.GamePhase#OVER}.
+     *
+     * <p>
+     * <strong>NOTE:</strong> The only instance variable that this method might change, if
+     * applicable, is ``phase``.
+     *
+     * <p>
+     * <strong>NOTE:</strong> If you want to use this method to determin a winner, then you must
+     * call it after each call to {@link #dropToken}.
+     *
+     * @return {@code true} if the last token dropped created a <em>connect four</em>, else
+     *     {@code false}
+     */
+    public boolean isLastDropConnectFour() {
+        //
+        // replace the entire contents of this method with your implementation
+        //
+        throw new UnsupportedOperationException("isWinner: not yet implemented.");
+    } // isLastDropConnectFour
+
+    //----------------------------------------------------------------------------------------------
+    // ADDITIONAL METHODS: If you create any additional methods, then they should be placed in the
+    // space provided below.
+    //----------------------------------------------------------------------------------------------
+
+
+
+
+    //----------------------------------------------------------------------------------------------
+    // DO NOT MODIFY THE METHODS BELOW!
+    //----------------------------------------------------------------------------------------------
+
+    /**
+     * <strong>DO NOT MODIFY:</strong>
+     * Print the game grid to standard output. This method assumes that the constructor
+     * is implemented correctly.
+     *
+     * <p>
+     * <strong>NOTE:</strong> This method should not be modified!
+     */
+    public void printGrid() {
+        TokenGrid.println(this.grid);
+    } // printGrid
+
+} // ConnectFour
